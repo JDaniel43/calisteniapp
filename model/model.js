e21bc5ac62e8ba81525c428b5d93dd01f507efb5
SantiagoCon <santiago@pidelectronics.com>
2022-10-29 12:34:20 -0600

    cambios

--- model/model.js
+++
@@ -1,6 +0,0 @@
-let connectionPool = require('../bin/bd_confi').connectionPool;
-
-exports.getMovies = () =>{
-    let query = 'select * from movies.movies';
-    return connectionPool.query(query)
-};
--- service/service.js
+++
@@ -1,4 +0,0 @@
-var model = require('../model/model');
-exports.getMovies = async ()  => {
-    return await model.getMovies()
-}
